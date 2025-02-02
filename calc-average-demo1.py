e792993768a8bf012cd51db18b0b80af07157490
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2025-02-02 08:49:08 -0500

    Update calc-average-demo1.py

--- calc-average-demo1.py
+++ calc-average-demo1.py
@@ -1,1 +1,17 @@
+# Calculate an average
+
+# Input
+quiz1 = 88
+quiz2 = 75
+num_scores = 2
+
+# Process
+total_points = quiz1 + quiz2
+average = total_points / num_scores
+
+# Output
+print(f'Score for Quiz 1: {quiz1}')
+print(f'Score for Quiz 2: {quiz2}')
+print(f'Average Score: {average}')
+
 
